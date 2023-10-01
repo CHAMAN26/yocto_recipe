cd37801b5c259246058ccb325b7b6bcfbaf159b4
CHAMAN26 <chaman.k@phytecembedded.in>
2023-10-01 23:36:19 +0530

    adding all files related to mycalculator recipe

---
+++ header.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+// Function declarations
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+
+#endif
+
---
+++ subtract.c
@@ -0,0 +1,6 @@
+#include "header.h"
+
+double subtract(double a, double b) {
+    return a - b;
+}
+
